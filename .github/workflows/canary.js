62f0d447ca880d6d31c6ed92f45eb0457f021f07
NeKz <NeKzor@users.noreply.github.com>
2024-11-13 03:11:42 +0100

    fix: release version and linux binary Co-authored-by: AMJ <69196954+ThisAMJ@users.noreply.github.com>

--- .github/workflows/canary.js
+++ .github/workflows/canary.js
@@ -7,7 +7,7 @@ const files = isWindows ? ["sar.dll", "sar.pdb"] : ["sar.so"];
 
 // @ts-check
 /** @param {import('@types/github-script').AsyncFunctionArguments} AsyncFunctionArguments */
-module.exports = async ({ context, core }) => {
+module.exports = async ({ release, context, core }) => {
   const git = spawnSync("git", ["describe", "--tags"]);
   if (git.status !== 0) {
     return git.stderr.toString("utf-8");
@@ -15,11 +15,11 @@ module.exports = async ({ context, core }) => {
 
   const SAR_VERSION = git.stdout.toString("utf-8");
 
-  core.info(`Uploading canary version ${SAR_VERSION}`);
+  core.info(`Uploading ${release ? 'release' : 'canary'} version ${SAR_VERSION}`);
 
   const body = new FormData();
-  body.set("version", `${SAR_VERSION.split("-").at(0)}-canary`);
-  body.set("sar_version", `${SAR_VERSION}-canary`);
+  body.set("version", `${SAR_VERSION.split("-").at(0)}${!release ? '-canary' : ''}`);
+  body.set("sar_version", `${SAR_VERSION}${!release ? '-canary' : ''}`);
   body.set("system", isWindows ? "windows" : "linux");
   body.set("commit", context.sha);
   body.set("branch", context.ref.split("/").at(-1));
@@ -27,7 +27,7 @@ module.exports = async ({ context, core }) => {
 
   for (let i = 0; i < files.length; ++i) {
     const filename = files[i];
-    const file = await readFile(`./bin/${filename}`);
+    const file = await readFile(`./${isWindows ? 'bin/' : ''}${filename}`);
     body.set(`hashes[${i}]`, createHash("sha256").update(file).digest("hex"));
     body.set(`files[${i}]`, new Blob([file]), filename);
   }
